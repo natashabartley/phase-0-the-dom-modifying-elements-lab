e8ab44ad819a090a218645de5355034f85a4e5f7
Natasha Bartley <+>
2023-04-16 21:21:40 -0400

    commit tree 590dd2e9a9191686e580fd0d762df05b890aa215

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+let main = document.getElementsByTagName(main);
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory")
+
+let yourName = "Natasha Bartley"
+newHeader.textContent(`${yourName} is the champion`)
+
+body.append(main, newHeader)
